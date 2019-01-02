ded52b6921e1ce0243d3c3c8bf9c53f4ce91ddb4
Arvinth <arvinth.manivasakan@external.sennheiser.com>
2019-01-02 11:31:07 +0530

    ios palyground

---
+++ MyPlayground45.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let name1 = "Arvinth"
+
+var name2: String?
+
+let name3 = name2 ?? name1
+
+print(name3)
+
+for i in 10...20 {
+  print(i)
+}
+
+let three = 3
+
+let minusthree = -three
+
+var sum = 10-minusthree
+
+sum += minusthree
+
+let names = ["Anna", "Alex", "Brian", "Jack"]
+
+
+for name in names[...2] {
+  print(name)
+}
+
+var clos = {(num) -> Int in
+  num + 1
+}
+
+print(clos(1))
+
+func closAsParam(cl: (Int)->Int) {
+  print(cl(1));
+}
+
+closAsParam(cl: clos)
+
+print(names.indices)
+
+for i in names.indices {
+  print(i)
+}
+
